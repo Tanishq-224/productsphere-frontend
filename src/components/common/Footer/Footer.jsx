b3bb8a8d3afc6e3b2c8494f0cbd2d30f7e38a726
Tanishq-224 <lashkaritanishq176@gmail.com>
2026-08-17 10:53:26 +0530

    Footer Updated

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   FiArrowUpRight,
+  FiChevronDown,
   FiMail,
   FiMapPin,
   FiPhone,
@@ -10,6 +12,15 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+
+  const [openSections, setOpenSections] = useState({});
+
+  const toggleSection = (section) => {
+    setOpenSections((prev) => ({
+      ...prev,
+      [section]: !prev[section],
+    }));
+  };
 
   return (
     <footer className={styles.footer}>
@@ -50,9 +61,13 @@ const Footer = () => {
             </Link>
           </div>
 
-          {/* LINKS */}
+          {/* =========================
+              DESKTOP LINKS
+          ========================= */}
 
           <div className={styles.linksWrapper}>
+
+            {/* COMPANY */}
 
             <div className={styles.linkColumn}>
               <h3>Company</h3>
@@ -70,6 +85,8 @@ const Footer = () => {
               </Link>
             </div>
 
+            {/* ACCOUNT */}
+
             <div className={styles.linkColumn}>
               <h3>Account</h3>
 
@@ -86,11 +103,14 @@ const Footer = () => {
               </Link>
             </div>
 
+            {/* CONTACT */}
+
             <div className={styles.linkColumn}>
               <h3>Contact</h3>
 
               <a href="mailto:hello@productsphere.com">
                 <FiMail />
+
                 <span>
                   hello@productsphere.com
                 </span>
@@ -98,6 +118,7 @@ const Footer = () => {
 
               <a href="tel:+919999999999">
                 <FiPhone />
+
                 <span>
                   +91 99999 99999
                 </span>
@@ -115,6 +136,167 @@ const Footer = () => {
             </div>
 
           </div>
+
+          {/* =========================
+              MOBILE ACCORDION
+          ========================= */}
+
+          <div className={styles.mobileLinks}>
+
+            {/* COMPANY */}
+
+            <div className={styles.mobileSection}>
+              <button
+                type="button"
+                className={`${styles.mobileHeader} ${
+                  openSections.company
+                    ? styles.mobileHeaderActive
+                    : ""
+                }`}
+                onClick={() =>
+                  toggleSection("company")
+                }
+              >
+                <span>Company</span>
+
+                <FiChevronDown
+                  className={
+                    openSections.company
+                      ? styles.chevronOpen
+                      : ""
+                  }
+                />
+              </button>
+
+              <div
+                className={`${styles.mobileContent} ${
+                  openSections.company
+                    ? styles.mobileContentOpen
+                    : ""
+                }`}
+              >
+                <Link to="/">
+                  Home
+                </Link>
+
+                <Link to="/products">
+                  Products
+                </Link>
+
+                <Link to="/profile">
+                  Profile
+                </Link>
+              </div>
+            </div>
+
+            {/* ACCOUNT */}
+
+            <div className={styles.mobileSection}>
+              <button
+                type="button"
+                className={`${styles.mobileHeader} ${
+                  openSections.account
+                    ? styles.mobileHeaderActive
+                    : ""
+                }`}
+                onClick={() =>
+                  toggleSection("account")
+                }
+              >
+                <span>Account</span>
+
+                <FiChevronDown
+                  className={
+                    openSections.account
+                      ? styles.chevronOpen
+                      : ""
+                  }
+                />
+              </button>
+
+              <div
+                className={`${styles.mobileContent} ${
+                  openSections.account
+                    ? styles.mobileContentOpen
+                    : ""
+                }`}
+              >
+                <Link to="/auth/login">
+                  Login
+                </Link>
+
+                <Link to="/auth/signup">
+                  Sign Up
+                </Link>
+
+                <Link to="/auth/forgot-password">
+                  Forgot Password
+                </Link>
+              </div>
+            </div>
+
+            {/* CONTACT */}
+
+            <div className={styles.mobileSection}>
+              <button
+                type="button"
+                className={`${styles.mobileHeader} ${
+                  openSections.contact
+                    ? styles.mobileHeaderActive
+                    : ""
+                }`}
+                onClick={() =>
+                  toggleSection("contact")
+                }
+              >
+                <span>Contact</span>
+
+                <FiChevronDown
+                  className={
+                    openSections.contact
+                      ? styles.chevronOpen
+                      : ""
+                  }
+                />
+              </button>
+
+              <div
+                className={`${styles.mobileContent} ${
+                  openSections.contact
+                    ? styles.mobileContentOpen
+                    : ""
+                }`}
+              >
+                <a href="mailto:hello@productsphere.com">
+                  <FiMail />
+
+                  <span>
+                    hello@productsphere.com
+                  </span>
+                </a>
+
+                <a href="tel:+919999999999">
+                  <FiPhone />
+
+                  <span>
+                    +91 99999 99999
+                  </span>
+                </a>
+
+                <div className={styles.mobileContactItem}>
+                  <FiMapPin />
+
+                  <span>
+                    Indore, Madhya Pradesh
+                    <br />
+                    India
+                  </span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
         </div>
 
         {/* =========================
